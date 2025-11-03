ecb4ccfb5274e046357a1b9b4486213c325e6a8a
Vinali Samarasinghe <vinalisamarasinghe@gmail.com>
2025-11-03 19:35:41 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
     public static void main(String[] args) {
-        CashOnDelivery COD = new CashOnDelivery(1000.00,"LKR","PENDING","no 5,main road,colombo");
-        BankTransfer BT = new BankTransfer(1500.00,"LKR","COMPLETED","BOC","13264978","444555");
-        CreditCardPayment CreditPay = new CreditCardPayment(45500.00,"LKR","FAILED","16453242","W.P.Perera","2029.10.14",10000.00);
-        DebitCardPayment DebitPay = new DebitCardPayment(3000.00,"LKR","PENDING","4652357","D.H.Senadeera","2022.12.12",8500.00);
+        CashOnDelivery COD = new CashOnDelivery(1000.00, "LKR", "PENDING", "no 5,main road,colombo");
+        BankTransfer BT = new BankTransfer(1500.00, "LKR", "COMPLETED", "BOC", "13264978", "444555");
+        CreditCardPayment CreditPay = new CreditCardPayment(45500.00, "LKR", "FAILED", "16453242", "W.P.Perera", "2029.10.14", 10000.00);
+        DebitCardPayment DebitPay = new DebitCardPayment(3000.00, "LKR", "PENDING", "4652357", "D.H.Senadeera", "2022.12.12", 8500.00);
 
 //        COD.processPayment();
 //        BT.processPayment();
@@ -27,14 +24,12 @@ public class Main {
         payments.add(CreditPay);
         payments.add(DebitPay);
 
-        for (Payment p: payments){
+        for (Payment p : payments) {
             p.processPayment();
             p.generateReceipt();
             System.out.println("***************");
         }
 
 
-
-
     }
-}
+
